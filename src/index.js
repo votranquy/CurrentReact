2ac76ef40a404238fede42b7311941c9cd8fb13c
Quy (Wyatt) T. VO <votranquy96@gmail.com>
2019-01-21 17:06:59 +0700

    Jan21th

--- src/index.js
+++ src/index.js
@@ -1054,91 +1054,294 @@ import './index.css';
 //======================================
 //Template 
 //==================================================================
-const scaleNames ={
-    c: 'Celsius',
-    f: 'Farenheit'
-};
+// const scaleNames ={
+//     c: 'Celsius',
+//     f: 'Farenheit'
+// };
 // function toCelsius(fahrenheit){
 //     return(fahrenheit -32)*5/9;
 // }
 // function toFahrenheit(celsius){
 //     return (celsius*9/5)+32;
 // }
-function tryConvert(temperature,scale){
-    const input = parseFloat(temperature);
-    if(Number.isNaN(input)){return '';}
-    const output = scale === 'f' ? (input-32)*5/9 : (input*9/5)+32 ;
-    const round = Math.round(output*1000) / 1000;
-    return round.toString();
-}
-function BoilingVerdict(props){
-    if(props.celsius >= 100) return <p>The water would boil.</p>
-    return <p>The water would not boil.</p>
-}
-class TemperatureInput extends React.Component{
-    constructor(props){
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-    }
-    handleChange(e){
-        this.props.onTemperatureInputChange(e.target.value);
-    }
+// function tryConvert(temperature,scale){
+//     const input = parseFloat(temperature);
+//     if(Number.isNaN(input)){return '';}
+//     const output = scale === 'f' ? (input-32)*5/9 : (input*9/5)+32 ;
+//     const round = Math.round(output*1000) / 1000;
+//     return round.toString();
+// }
+// function BoilingVerdict(props){
+//     if(props.celsius >= 100) return <p>The water would boil.</p>
+//     return <p>The water would not boil.</p>
+// }
+// class TemperatureInput extends React.Component{
+//     constructor(props){
+//         super(props);
+//         this.handleChange = this.handleChange.bind(this);
+//     }
+//     handleChange(e){
+//         this.props.onTemperatureInputChange(e.target.value);
+//     }
+//     render(){
+//         const temperature = this.props.temperature;
+//         const scale = this.props.scale;
+//         return(
+//             <fieldset>
+//                 <legend>Enter temperature in {scaleNames[scale]}:</legend>
+//                 <input value={temperature} onChange={this.handleChange}/>
+//             </fieldset>
+//         );
+//     }
+// }
+// class Calculator extends React.Component{
+//     constructor(props){
+//         super(props);
+//         this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+//         this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+//         this.state = {
+//             temperature: '0',
+//             scale: 'c',
+//         };
+//     }
+//     handleCelsiusChange(temperature){
+//         this.setState({
+//             scale: 'c',
+//             temperature
+//         });
+//     }
+//     handleFahrenheitChange(temperature){
+//         this.setState({
+//             scale: 'f',
+//             temperature
+//         });
+//     }
+//     render(){
+//         const scale = this.state.scale;
+//         const temperature = this.state.temperature;
+//         const celsius = scale === 'f' ? tryConvert(temperature,scale)  : temperature;
+//         const fahrenheit = scale === 'c' ? tryConvert(temperature,scale) : temperature;
+//         return(        
+//             <div>
+//                 <TemperatureInput 
+//                     scale="c" 
+//                     temperature={celsius} 
+//                     onTemperatureInputChange={this.handleCelsiusChange}
+//                 />
+//                 <TemperatureInput 
+//                     scale="f" 
+//                     temperature={fahrenheit} 
+//                     onTemperatureInputChange={this.handleFahrenheitChange}
+//                 />
+//                 <BoilingVerdict celsius={parseFloat(celsius)}/>
+//             </div>
+//         );
+//     }
+// }
+// ReactDOM.render(<Calculator />,document.getElementById('root'));
+//https://reactjs.org/docs/lifting-state-up.html
+//The inputs stay in sync because their values are computed from the same state:
+//https://reactjs.org/docs/composition-vs-inheritance.html
+
+//==================================================================
+//Composition and Inheritance
+//==================================================================
+// function FancyBorder(props){
+//     return(
+//         <div className={'FancyBorder FancyBorder-'+props.color}>
+//             {props.children}
+//         </div>
+//     );
+// }
+// function WelcomeDialog(){
+//     return(
+//         <FancyBorder color="blue">
+//         {/* children */}
+//             <h1 className="Dialog-title">
+//                 Welcome
+//             </h1>
+//             <p className="Dialog-message">
+//                 Thank you.
+//             </p>
+//         {/* .children     */}
+//         </FancyBorder>
+//     );
+// }
+// ReactDOM.render(<WelcomeDialog/>,document.getElementById('root'));
+//===========================
+//Hole in component
+//=================================
+// function Contacts() {
+//     return <div className="Contacts" />;
+// } 
+
+// function Chat() {
+//     return <div className="Chat" />;
+// }
+
+// function SplitPane(props){
+//     return(
+//         <div className="SplitPane">
+//             <div className="SplitPane-left">
+//                 {props.left}
+//             </div>
+//             <div className="SplitPane-right">
+//                 {props.right}
+//             </div>
+//         </div>
+//     );
+// }
+// function App(){
+//     return(
+//         <SplitPane left={<Contacts/>} right={<Chat/>} />
+//     );
+// }
+// ReactDOM.render(<App/>,document.getElementById('root'));
+
+//================================
+//================================
+// 
+ //==============================
+ //======================================
+// function FancyBorder(props){
+//     return(
+//         <div className={'FancyBorder FancyBorder-'+props.color}>
+//             {props.children}
+//         </div>
+//     );
+// }
+//  function Dialog(props){
+//      return(
+//          <FancyBorder color="blue">
+//             <h1 className="Dialog-title">
+//                 {props.title}
+//             </h1>
+//             <p className="Dialog-message">
+//                 {props.message}
+//             </p>
+//             {props.children}
+//          </FancyBorder>
+//      );
+//  }
+//  class SignUpDialog extends React.Component{
+//      constructor(props){
+//          super(props);
+//          this.handleChange = this.handleChange.bind(this);
+//          this.handleSignUp = this.handleSignUp.bind(this);
+//          this.state = {login: ''};
+//      }
+//      render(){
+//          return(
+//              <Dialog title="Mars Exploration Program" message=" How should we refer to you">
+//                 <input value={this.state.login} onChange={this.handleChange} />
+//                 <button onClick={this.handleSignUp}>Sign Up</button>
+//              </Dialog>
+//          );
+//      }
+//      handleChange(e){
+//          this.setState({login: e.target.value});
+//      }
+//      handleSignUp(){
+//          alert(`Welcome abroad,  ${this.state.login}`);
+//      }
+//  }
+//  ReactDOM.render(<SignUpDialog/>, document.getElementById('root'));
+//========================================
+//Thinking In React
+//========================================
+class ProductCategoryRow extends React.Component{
     render(){
-        const temperature = this.props.temperature;
-        const scale = this.props.scale;
+        const category = this.props.category;
         return(
-            <fieldset>
-                <legend>Enter temperature in {scaleNames[scale]}:</legend>
-                <input value={temperature} onChange={this.handleChange}/>
-            </fieldset>
+            <tr>
+                <th colSpan="2">
+                    {category}
+                </th>
+            </tr>
         );
     }
 }
-class Calculator extends React.Component{
-    constructor(props){
-        super(props);
-        this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
-        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
-        this.state = {
-            temperature: '0',
-            scale: 'c',
-        };
-    }
-    handleCelsiusChange(temperature){
-        this.setState({
-            scale: 'c',
-            temperature
-        });
-    }
-    handleFahrenheitChange(temperature){
-        this.setState({
-            scale: 'f',
-            temperature
-        });
-    }
+class ProductRow extends React.Component{
     render(){
-        const scale = this.state.scale;
-        const temperature = this.state.temperature;
-        const celsius = scale === 'f' ? tryConvert(temperature,scale)  : temperature;
-        const fahrenheit = scale === 'c' ? tryConvert(temperature,scale) : temperature;
-        return(        
+        const product = this.props.product;
+        const name = product.stocked ?
+            product.name :
+            <span style={{color:'red'}}>
+                {product.name}
+            </span>;
+        return(
+            <tr>
+                <td>{name}</td>
+                <td>{product.price}</td>
+            </tr>
+        );
+    }
+}
+class ProductTable extends React.Component{
+    render(){
+        const rows=[];
+        let lastCategory=null;
+        this.props.products.forEach((product)=>{
+            if(product.category !== lastCategory){
+                rows.push(
+                    <ProductCategoryRow 
+                        category={product.category}
+                        key={product.category}/>
+                );
+            }
+            rows.push(
+                <ProductRow 
+                    product={product}
+                    key={product.name}/>
+            );
+            lastCategory = product.category;
+        });
+        return(
+            <table>
+                <thead>
+                    <tr>Name</tr>
+                    <tr>Price</tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
+        );
+    }
+}
+class SearchBar extends React.Component{
+    render(){
+        return(
+            <form>
+                <input type="text" placeholder="Search ....."/>
+                <p>
+                    <input type="checkbox"/>
+                    {''}
+                    Only show products in stock
+                </p>
+            </form>
+        );
+    }
+}
+class FilterableProductTable extends React.Component{
+    render(){
+        return(
             <div>
-                <TemperatureInput 
-                    scale="c" 
-                    temperature={celsius} 
-                    onTemperatureInputChange={this.handleCelsiusChange}
-                />
-                <TemperatureInput 
-                    scale="f" 
-                    temperature={fahrenheit} 
-                    onTemperatureInputChange={this.handleFahrenheitChange}
-                />
-                <BoilingVerdict celsius={parseFloat(celsius)}/>
+                <SearchBar/>
+                <ProductTable products={this.props.products}/>
             </div>
         );
     }
 }
-ReactDOM.render(<Calculator />,document.getElementById('root'));
-//https://reactjs.org/docs/lifting-state-up.html
-//The inputs stay in sync because their values are computed from the same state:
-//https://reactjs.org/docs/composition-vs-inheritance.html
+const PRODUCTS =[
+    {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+    {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+    {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+    {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+    {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+    {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+ReactDOM.render(
+    <FilterableProductTable products={PRODUCTS}/>, document.getElementById('root')
+);
+//https://reactjs.org/docs/thinking-in-react.html
