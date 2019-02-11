05718b7db8eb96e76aa2f6a041cdcd8a45626d2e
VoTranQuyWyatt <wyatt@enclave.vn>
2019-02-11 17:01:55 +0700

    code splitting

--- src/index.js
+++ src/index.js
@@ -2045,54 +2045,69 @@ import './index.css';
 //
 //=================================================================
 
-class BlurExample extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={isOpen: false};
-        this.timeOutId = null;
-        this.onClickHandler = this.onClickHandler.bind(this);
-        this.onBlurHandler =this.onBlurHandler.bind(this);
-        this.onFocusHandler = this.onFocusHandler.bind(this);
-    }
-    onClickHandler(){
-        this.setState(currentState => ({
-            isOpen: !currentState.isOpen
-        }));
-    }
-    onBlurHandler(){
-        this.timeOutId = setTimeout(()=>{
-            this.setState({
-                isOpen:false    
-            });
-        });
-    }
-    onFocusHandler(){
-        clearTimeout(this.timeOutId);
-    }
-    render(){
-        return(
-            <div 
-                onBlur={this.onBlurHandler}
-                onFocus={this.onFocusHandler}
-            >
-                <button 
-                    onClick={this.onClickHandler}
-                    aria-haspopup="true"
-                    aria-expanded={this.state.isOpen}
-                >
-                    Select an option
-                </button>
-                {this.state.isOpen ? (
-                    <ul>
-                        <li>Option 1</li>
-                        <li>Option 2</li>
-                        <li>Option 3</li>
-                    </ul>
-                ) :null}
-                <button>A</button>
-                <button>B</button>
-            </div>
-        );
-    }
+// class BlurExample extends React.Component{
+//     constructor(props){
+//         super(props);
+//         this.state={isOpen: false};
+//         this.timeOutId = null;
+//         this.onClickHandler = this.onClickHandler.bind(this);
+//         this.onBlurHandler =this.onBlurHandler.bind(this);
+//         this.onFocusHandler = this.onFocusHandler.bind(this);
+//     }
+//     onClickHandler(){
+//         this.setState(currentState => ({
+//             isOpen: !currentState.isOpen
+//         }));
+//     }
+//     onBlurHandler(){
+//         this.timeOutId = setTimeout(()=>{
+//             this.setState({
+//                 isOpen:false    
+//             });
+//         });
+//     }
+//     onFocusHandler(){
+//         clearTimeout(this.timeOutId);
+//     }
+//     render(){
+//         return(
+//             <div 
+//                 onBlur={this.onBlurHandler}
+//                 onFocus={this.onFocusHandler}
+//             >
+//                 <button 
+//                     onClick={this.onClickHandler}
+//                     aria-haspopup="true"
+//                     aria-expanded={this.state.isOpen}
+//                 >
+//                     Select an option
+//                 </button>
+//                 {this.state.isOpen ? (
+//                     <ul>
+//                         <li>Option 1</li>
+//                         <li>Option 2</li>
+//                         <li>Option 3</li>
+//                     </ul>
+//                 ) :null}
+//                 <button>A</button>
+//                 <button>B</button>
+//             </div>
+//         );
+//     }
+// }
+// ReactDOM.render(<BlurExample/>,document.getElementById('root'));
+
+//=================================================================
+//
+//=================================================================
+
+const OtherComponent = React.lazy( ()=> import('./OtherComponent'));
+function MyComponent() {
+    return(
+        <div>
+            <OtherComponent />
+        </div>
+    );
 }
-ReactDOM.render(<BlurExample/>,document.getElementById('root'));
+ReactDOM.render(<MyComponent/>, document.getElementById('root'));
+//https://reactjs.org/docs/code-splitting.html
