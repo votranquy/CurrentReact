2daa8b55ed2d838f24930754a82a63ea73c72c94
VoTranQuyWyatt <wyatt@enclave.vn>
2019-02-25 15:34:39 +0700

    Completed Seach Box

--- src/App.js
+++ src/App.js
@@ -1,88 +1,14 @@
-// import React from 'react';
-// import productsData from "./vschoolProducts.js";
-// import Product from "./Product";
-
-// function App(){
-//     const productComponents = productsData.map( item => <Product key={item.id} product={item} /> );
-//     return(
-//         <div>
-//             {productComponents}
-//         </div>
-//     );
-// }
-
-// export default App;
-
-
-//=================================================================
-//
-//=================================================================
-
-// import React from "react";
-// // import TodoItem from "./TodoItem.js";
-// // import todosData from "./todosData.js";
-
-// const todosData = [
-//     {
-//         id:1,
-//         text: "Take out the trash",
-//         completed:true
-//     },
-//     {
-//         id:2,
-//         text: "Go shopping",
-//         completed:false
-//     },
-//     {
-//         id:3,
-//         text: "Mow lawn",
-//         completed:true
-//     },
-//     {
-//         id:4,
-//         text: "Watch Hallay show",
-//         completed:false
-//     },
-// ];
-
-// function TodoItem(props){
-//     return(
-//         <div className="todo-item">
-//             <input type="checkbox" checked={props.item.completed} />
-//             <p> {props.item.text} </p>
-//         </div>
-//     );
-// }
-
-// function App(){
-
-//     const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/> )
-//     return(
-//         <div className="todo-list">
-//             {todoItems}
-//         </div>
-//     );
-
-// }
-
-// export default App;
-
-//=================================================================
-//
-//=================================================================
-
-
 import React from "react";
-import HideableText from "./HideableText.js";
-import AutoCompleteText from './AutoCompleteText.js';
+import AutoCompleteText from './AutoCompleteText';
+
 class App extends React.Component{
     render(){
         return(
-            <div className="todo-list">
-                <HideableText text="Hello"/>
+            <div className="App">
                 <AutoCompleteText />
             </div>
         );
     }
 }
+
 export default App;
--- src/AutoCompleteText.js
+++ src/AutoCompleteText.js
@@ -1,50 +1,67 @@
 import React from 'react';
 
-export default class AutoCompleteText extends React.Component{
+class AutoCompleteText extends React.Component{
 
     constructor(props){
         super(props);
-        this.items =[
+        this.items = [
             'David',
             'Jane',
             'Jara',
             'Tavia',
+            'Danien',
+            'Sara',
+            'Wyatt',
+            'Mathew',
+            'Halliwel'
         ];
         this.state = {
             suggestions: [],
+            text:''
         };
     }
 
-    onTextChanged =(e) => {
+    onTextChanged = (e) => {
         const value = e.target.value;
-        let suggestions =[];
+        let suggestions = [];
+
         if(value.length > 0){
             const regex = new RegExp(`^${value}`,'i');
-            suggestions = this.items.sort().filter(v => this.setState(regex))
+            suggestions = this.items.sort().filter(v => regex.test(v));
         }
-        this.setState(() => ({suggestions}));
+        this.setState(() => ({suggestions, text: value}));
+    }
+
+    suggestionSelected(value){
+        this.setState( ()=> ({
+            text:value,
+            suggestions: [],
+        }))
     }
 
     renderSuggestions(){
-        const {suggestions} = this.state;
-        if(suggestion.length === 0){
+        const { suggestions } = this.state;
+        if(suggestions.length === 0){
             return null;
         }
         return(
             <ul>
-                {this.suggestions.map( (item) => <li>{item}</li> )}  
+                {suggestions.map( (item) => <li onClick={()=> this.suggestionSelected(item)}>{item}</li> )}  
             </ul>
         );
     }    
+
     render(){
+        const {text} = this.state;
         return(
             <div>
-                <input onChange={(e) => console.log(e.target.value)} type="text" />
+                <input value={text} onChange={ this.onTextChanged } type="text" />
                 <ul>
-                    {this.renderSuggestions()}                 }
+                    {this.renderSuggestions()}
                 </ul>
             </div>
         );
     }
-
 }
+
+export default AutoCompleteText;
